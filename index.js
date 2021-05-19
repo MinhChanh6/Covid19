1466aec08b2e27138ae1a4b62e9e30f3593b8311
MinhChanh6 <hominhchanh08@gmail.com>
2021-05-19 19:41:43 +0700

    Update

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 
-//********Active-link menu********// 
+//******** Active-link menu********// 
 
 const sections = document.querySelectorAll('section[id]')
 const navLink = document.querySelectorAll('.header-menu li')
 console.log(navLink)
 function scrollActive(){
     const scrollY = window.pageYOffset
-    console.log(scrollY)
     sections.forEach(section =>{
         const sectionHeight = section.offsetHeight;
         const sectionTop = section.offsetTop;
         if(scrollY >= (sectionTop - sectionHeight/3)){
             sectionId = section.getAttribute('id')
         }
-        console.log(sectionId)
     })
     navLink.forEach(li => {
         li.classList.remove('active-link')
@@ -40,36 +38,16 @@ window.addEventListener('scroll', changeBackground)
 
 //******** Animations ********// 
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
-const tl1 = gsap.timeline ({
-    srcollTrigger: {
-        trigger: '.todo',
-        start: 'center bottom',
-        pin: 'true'
-    }
-});
-tl1.from(".todo-img", {x: 200, opacity: 0 , duration:1.5})
-    .from(".todo-text",{y: 300, opacity:0 , duration: 1}, "=-1");
-tl.to(".text", { y: "0%", duration: 1.3, stagger: 0.25 });
-tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
-tl.to(".intro", { y: "-100%", duration: 1.5 }, "-=1.5");
+tl.to(".text",{y: "0%", duration: 1, stagger: 0.5});
+tl.to(".slider", { y: "-100%", duration: 1, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1  }, "-=1");
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo(".content", { opacity: 0 }, { opacity: 1, duration: 1 });
 
 
 //******** Smooth-Sroll ********// 
-// var scroll = new SmoothScroll('.nav a[href*="#"]',{
-//     speed: 700
-// });
-
-var scroll = new SmoothScroll('.nav a[href*="#"]', {
-	// Function. Custom easing pattern
-	// If this is set to anything other than null, will override the easing option above
-	customEasing: function (time) {
-
-		// return <your formulate with time as a multiplier>
-
-		// Example: easeInOut Quad
-		return time < 0.5 ? 2 * time * time : -1 + (4 - 2 * time) * time;
-
-	}
+var scroll = new SmoothScroll('.nav a[href*="#"]',{
+    speed: 650
 });
+
+
